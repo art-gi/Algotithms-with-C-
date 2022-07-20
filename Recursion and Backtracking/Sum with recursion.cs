1558565f9f9145a23f4dfec3af54b1ebd843cfd8
art-gi <artgi@mail.bg>
2022-07-20 10:04:43 +0300

    Sum with Recursion

---
+++ Recursion and Backtracking/Sum with recursion.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursion_and_Backtracking
+{
+    class Program
+    {
+        public static void Main()
+        {
+            var arr = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            Console.WriteLine(SumRecursively(arr, 0));
+        }
+        private static int SumRecursively(int[] arr, int index)
+        {
+            if (index >= arr.Length - 1)
+            {
+                return arr[index];
+            }
+
+            return arr[index] + SumRecursively(arr, index + 1);
+        }
+        
+    }
+   
+}
